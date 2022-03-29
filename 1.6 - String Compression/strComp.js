07bdd65834a1c5cd5b5fca3300bbe891486776ef
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-03-29 22:44:57 +0100

    string compression

--- 1.6 - String Compression/strComp.js
+++ 1.6 - String Compression/strComp.js
@@ -1,25 +1,38 @@
-var strComp = function(string) {
-  var compressed = '';
-  var currChar = '';
-  var currCount = '';
-  var maxCount = 1;
-  for (var i = 0; i < string.length; i++) {
-    if (currChar !== string[i]) {
-      console.log(currChar, string[i], i);
-      compressed = compressed + currChar + currCount;
-      maxCount = Math.max(maxCount, currCount);
-      currChar = string[i];
-      currCount = 1;
+/*
+
+String Compression: Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+Hints:
+Do the easy thing first. Compress the string, then compare the lengths.
+Be careful that you aren't repeatedly concatenating strings together. This can be very
+inefficient.
+
+*/
+
+const strComp = (str) => {
+  let compressedString = "";
+  let currentCount = "";
+  let currentChar = "";
+  let maxCount = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    if (currentChar !== str[i]) {
+      compressedString = compressedString + currentChar + currentCount;
+      maxCount = Math.max(maxCount, currentCount);
+      currentChar = str[i];
+      currentCount = 1;
     } else {
-      currCount++;
+      currentCount++;
     }
   }
-  compressed = compressed + currChar + currCount;
-  maxCount = Math.max(maxCount, currCount);
-
-  return maxCount === 1 ? string : compressed;
+  compressedString = compressedString + currentChar + currentCount;
+  maxCount = Math.max(maxCount, currentCount);
+  return maxCount !== 1 ? compressedString : str;
 };
 
 // Test
-console.log('aaaaaa', strComp('aaaaaa'), 'a6');
-console.log('aabcccccaaa', strComp('aabcccccaaa'), 'a2b1c5a3');
+console.log("aaaaaa", strComp("aaaaaa"), "a6");
+console.log("aabcccccaaa", strComp("aabcccccaaa"), "a2b1c5a3");
