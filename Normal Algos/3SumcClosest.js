0f2a6a02030ff21ae743ea10b7f2be4315b21fad
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-04-23 22:31:39 +0100

    three sum smaller and closest

--- Normal Algos/3SumcClosest.js
+++ Normal Algos/3SumcClosest.js
@@ -16,6 +16,50 @@ Example 2:
 
 Input: nums = [0,0,0], target = 1
 Output: 0
-*/
+ 
 
-const threeSumClosest = (nums, target) => {};
+Constraints:
+3 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+-104 <= target <= 104
+
+*/
+const threeSumClosest = (nums, target) => {
+  nums.sort((a, b) => a - b);
+  let bestSum = Number.MAX_SAFE_INTEGER;
+  let leftPointer, rightPointer;
+  //loop through each element
+
+  for (let [index, number] of nums.entries()) {
+    if (index > 0 && number === nums[index - 1]) continue;
+
+    leftPointer = index + 1;
+    rightPointer = nums.length - 1;
+
+    while (leftPointer < rightPointer) {
+      let threeSum = number + nums[leftPointer] + nums[rightPointer];
+
+      if (threeSum === target) {
+        return target;
+      }
+
+      if (Math.abs(target - threeSum) < Math.abs(target - bestSum)) {
+        bestSum = threeSum;
+      }
+
+      if (threeSum <= target) {
+        leftPointer++;
+
+        while (
+          nums[leftPointer] === nums[leftPointer - 1] &&
+          leftPointer < rightPointer
+        ) {
+          leftPointer++;
+        }
+      } else {
+        rightPointer--;
+      }
+    }
+  }
+  return bestSum;
+};
