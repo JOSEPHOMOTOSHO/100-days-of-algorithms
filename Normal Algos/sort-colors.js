af165bb53040aa7505b8388447c4f9907f5cf6aa
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-04-25 22:19:52 +0100

    completed

--- Normal Algos/sort-colors.js
+++ Normal Algos/sort-colors.js
@@ -25,4 +25,26 @@ nums[i] is either 0, 1, or 2.
  
 */
 
-const sortColors = (nums) => {};
+const sortColors = (nums) => {
+  let start = 0;
+  let end = nums.length - 1;
+  let index = 0;
+
+  while (index <= end && start <= end) {
+    if (nums[index] === 0) {
+      nums[index] = nums[start];
+      nums[start] = 0;
+      start++;
+      index++;
+    } else if (nums[index] === 2) {
+      nums[index] = nums[end];
+      nums[end] = 2;
+      end--;
+    } else {
+      index++;
+    }
+  }
+  return nums;
+};
+
+console.log(sortColors([2, 0, 1]), [0, 1, 2]);
