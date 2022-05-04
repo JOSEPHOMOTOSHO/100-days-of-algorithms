5406f83c8ab653f75c054b2af9b4e4e633838afc
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-04 23:37:08 +0100

    Reorder list

--- Normal Algos/ReorderList.js
+++ Normal Algos/ReorderList.js
@@ -8,7 +8,7 @@ L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
 
-https://leetcode.com/problems/reorder-list/
+https://leetcode.com/problems/reorder-list/.
 */
 
 /**
