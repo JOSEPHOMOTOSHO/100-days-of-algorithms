4b18efd2a8e68088986d3f474dc666bbc74e8241
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-13 23:28:32 +0100

    valid parens

--- Normal Algos/BinarySearch.js
+++ Normal Algos/BinarySearch.js
@@ -1,18 +1,20 @@
 
 
 const search = (nums, target) => {
-    let half = ~~(nums.length / 2)
     let left = 0
     let right = nums.length - 1
-    while(left < right && nums[half] !== target){
-        if(nums[half] > target  ){
-            right =  half - 1
-        }else if(nums[half] < target  ){
-             left =  half + 1
+    while(left <= right ){
+        let half = left + ~~((right - left ) / 2)
+        half
+        if(nums[half] === target){
+            return half
+        }else if(nums[half] > target){
+            right = half - 1
+        }else{
+            left = half + 1
         }
-        half = ~~((right - left) / 2)
     }
-    return nums[half] === target ? nums.indexOf(nums[half]) : -1
+   return -1
 };
 
 console.log((search([-1,0,3,5,9,12],9)));
