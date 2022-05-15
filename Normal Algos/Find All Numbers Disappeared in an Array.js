aedd201631e12eec25d3ec42ab1c172d8a041493
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-15 22:52:53 +0100

    find all disappeared missing numbers

---
+++ Normal Algos/Find All Numbers Disappeared in an Array.js
@@ -0,0 +1,49 @@
+/*
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+ 
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ const findDisappearedNumbers = (nums) => {
+    //if one is subtracted from each element or number in the array,it can be used as an          
+     //index. This Index points to a position in the array.
+    //[we do this coz the input array is scattered]
+    //if a position is never pointed (or marked) at in the array. That position + 1 gives the     
+    //missing the number
+    for(let i = 0; i<nums.length; i++){
+        let numConvertedToIndex = Math.abs(nums[i]) - 1
+         nums[numConvertedToIndex] = Math.abs(nums[numConvertedToIndex]) * -1
+    }
+    
+    let result = []
+    
+    for(let i = 0; i < nums.length; i++ ){
+        if(nums[i] > 0){
+            result.push(i+1)
+        }
+    }
+    return result
+};
+
+console.log(findDisappearedNumbers( [4,3,2,7,8,2,3,1]),[5,6]);
