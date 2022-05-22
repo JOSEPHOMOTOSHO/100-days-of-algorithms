a3fb9e0f24c465c52120d3cc39b0a84147837909
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-22 21:41:19 +0100

    start

---
+++ Normal Algos/Reverse Linked List II.js
@@ -0,0 +1,22 @@
+/*
+Given the head of a singly linked list and two integers left and right where left <= right, 
+reverse the nodes of the list from position left to position right, and return the reversed list.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+ var reverseBetween = function(head, left, right) {
+    
+};
