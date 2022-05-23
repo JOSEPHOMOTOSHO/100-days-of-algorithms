dc248b0209b20ea2ea7a66c5bae325736044a641
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-23 22:47:09 +0100

    not done

--- Normal Algos/Reverse Linked List II.js
+++ Normal Algos/Reverse Linked List II.js
@@ -17,6 +17,69 @@ reverse the nodes of the list from position left to position right, and return t
  * @param {number} right
  * @return {ListNode}
  */
- var reverseBetween = function(head, left, right) {
+ /**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+    if(left === right) return head
+    if (!head || !head.next) return head;
+    //have two pointers
+    let leftPointer = 1
+    let rightPointer = 1
+    let leftNode = head
+    let rightNode = head
+    let nodeBeforeLeftNode = head
+    let nodeAfterRightNode
+    while(leftPointer < left){
+        if(left - leftPointer === 1){
+             nodeBeforeLeftNode = leftNode
+        }
+        leftNode = leftNode.next
+        leftPointer++
+    }
     
+     while(rightPointer < right){
+        rightNode = rightNode.next
+        rightPointer++
+    }
+    
+ nodeAfterRightNode = rightNode.next
+    
+    //reverse the sub portion
+    let prev = null
+    let leftNodeHold = leftNode
+    while(leftNode && leftNode !== nodeAfterRightNode){
+        let next = leftNode.next
+        leftNode.next = prev
+        prev = leftNode
+        leftNode = next
+    }
+nodeBeforeLeftNode.next = rightNode
+leftNodeHold.next = nodeAfterRightNode
+ 
+return nodeBeforeLeftNode
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
