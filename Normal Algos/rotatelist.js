e4ff6f230a13bdf5963720914ff835e50c19a1da
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-27 23:05:52 +0100

    add

---
+++ Normal Algos/rotatelist.js
@@ -0,0 +1,20 @@
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+https://leetcode.com/problems/rotate-list/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+ const rotateRight = (head, k) => {
+    
+};
