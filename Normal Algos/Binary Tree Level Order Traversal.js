2eed68d9736063969bfc5d037c4a40cea2a89fd4
Omotosho Joseph <joseph.omotosho98@gmail.com>
2022-05-30 23:33:19 +0100

    add

---
+++ Normal Algos/Binary Tree Level Order Traversal.js
@@ -0,0 +1,20 @@
+/*
+Given the root of a binary tree, return the level order traversal of its nodes' values.
+ (i.e., from left to right, level by level).
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+ const levelOrder = (root) => {
+     
+    
+};
